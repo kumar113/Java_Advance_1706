79ba6343e77b773ff0fd4894ae1cf049dab82acd
kumar113 <aman.kumar@knoldus.com>
2022-02-11 20:09:32 +0530

    Program 11

---
+++ Test11.java
@@ -0,0 +1,23 @@
+
+class Outer { 
+    void outerMethod() { 
+    System.out.println("Inside outerMethod"); 
+    // Inner class is local to outerMethod() 
+        class Inner { 
+            void innerMethod() { 
+            System.out.println("Inside innerMethod"); 
+            } 
+        } 
+        Inner y = new Inner(); 
+        y.innerMethod(); 
+    } 
+} 
+class Test11{ 
+    public static void main(String[] args) { 
+    Outer x = new Outer(); 
+    x.outerMethod(); 
+    } 
+}
+
+//Inside outerMethod
+//Inside innerMethod
